967e4a450b7c55341128df0be3e6addcdf013101
Felipe Rando <felipe.rando@rivendel.com.br>
2017-08-02 11:57:17 -0300

    debugger_activated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route("/")
 @app.route("/home")
@@ -10,6 +11,17 @@ def hello_world():
 @app.route("/felipe")
 def dono():
     return "Meu nome eh felipe"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael" :
+        return "Hello, {}".format(name), 200
+    else :
+        return "Not Found", 404
 
 if __name__ == "__main__":
     app.run()
